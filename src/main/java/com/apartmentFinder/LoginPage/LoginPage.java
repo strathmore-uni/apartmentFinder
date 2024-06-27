755467f059ed34e2b8ef941a91c4af2c908a7f6f
OTogano <144037469+OTogano@users.noreply.github.com>
2024-06-27 17:34:58 +0300

    Added design for loginPage

--- src/main/java/com/apartmentFinder/LoginPage/LoginPage.java
+++ src/main/java/com/apartmentFinder/LoginPage/LoginPage.java
@@ -2,15 +2,33 @@ package com.apartmentFinder.LoginPage;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 public class LoginPage {
     private Container container;
     private CardLayout cardLayout;
     private JPanel mainPanel;
+    private JButton button1;
+    private JTextField EnterPhoneNumber;
 
     public LoginPage(Container container, CardLayout cardLayout){
         this.container = container;
         this.cardLayout = cardLayout;
+        EnterPhoneNumber.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (EnterPhoneNumber.getText().equals("e.g 0712345678")) {
+                    EnterPhoneNumber.setText("");
+                }
+            }
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (EnterPhoneNumber.getText().isEmpty()) {
+                    EnterPhoneNumber.setText("e.g 0712345678");
+                }
+            }
+        });
     }
 
     public JPanel createMainPanel(){
