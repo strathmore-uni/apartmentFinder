d693560e5f3de20f88f05bfe4d8d6ec194293905
Braine Lomoni <82029545+code-briomar@users.noreply.github.com>
2024-06-29 18:29:40 +0300

    design : Choice Page

--- src/main/java/ApartmentFinder.java
+++ src/main/java/ApartmentFinder.java
@@ -45,8 +45,9 @@ public class ApartmentFinder extends JFrame  {
         container.add("templatePage",template.getMainPanel());
 
         // Set the first page to display - choicePage
-//        cardLayout.show(container,"choicePage");
- cardLayout.show(container,"templatePage");
+        cardLayout.show(container,"choicePage");
+        // Template Page
+        //        cardLayout.show(container,"templatePage");
         setContentPane(container);
 
         setSize(900,600);
--- src/main/java/com/apartmentFinder/Choice/ChoicePage.java
+++ src/main/java/com/apartmentFinder/Choice/ChoicePage.java
@@ -7,6 +7,12 @@ public class ChoicePage {
     private Container container;
     private CardLayout cardLayout;
     private JPanel mainPanel;
+    private JLabel title;
+    private JLabel subtitle;
+    private JButton addApartmentButton;
+    private JButton button1;
+    private JTextField searchALocation;
+    private JComboBox comboBox1;
 
     public ChoicePage(Container container, CardLayout cardLayout){
         this.container = container;
