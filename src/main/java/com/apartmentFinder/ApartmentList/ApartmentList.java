86ec003560617358f97a13e5063f8d6066046c3b
Braine Lomoni <82029545+code-briomar@users.noreply.github.com>
2024-07-10 00:29:49 +0300

    feature : ApartmentList Page functionalities

--- src/main/java/com/apartmentFinder/ApartmentList/ApartmentList.java
+++ src/main/java/com/apartmentFinder/ApartmentList/ApartmentList.java
@@ -1,6 +1,7 @@
 package com.apartmentFinder.ApartmentList;
 
 import com.apartmentFinder.ApartmentList.components.ApartmentCard;
+import com.apartmentFinder.ApartmentList.components.ApartmentCardJava;
 import com.apartmentFinder.components.utils.DBConnector;
 import com.apartmentFinder.components.utils.SearchData;
 import com.apartmentFinder.components.utils.Unit;
@@ -19,9 +20,13 @@ public class ApartmentList {
     private JButton button1;
     private JComboBox max_price_combo_box;
     private JPanel apartmentListContainer;
+    private JPanel apartmentCards;
     private String location_value;
     private String min_price;
     private String max_price;
+
+    // Store data fetched from the database
+    private LinkedList<Unit> apartmentList;
 
     public ApartmentList(Container container, CardLayout cardLayout){
         this.container = container;
@@ -44,34 +49,105 @@ public class ApartmentList {
 
         // Fetch data with filters from the database
         DBConnector dbConnector = new DBConnector();
-        LinkedList<Unit> apartmentList = dbConnector.fetchFilteredApartments(location_value,min_price, max_price);
+        this.apartmentList = dbConnector.fetchFilteredApartments(location_value,min_price, max_price);
+
+        // Update the apartment cards
+        refreshApartmentCards();
 
         // Display apartmentListContainer cards
-        ApartmentCard[] cards = new ApartmentCard[apartmentList.size()];
-        int counter = 0;
-        for (Unit unit : apartmentList) {
-            // Apartment ID
-            int apartmentID = Integer.parseInt(unit.getApartmentId());
-            int unitID = Integer.parseInt(unit.getUnitId());
-            cards[counter] = new ApartmentCard(apartmentID,unitID);
-            // Location Description
-            cards[counter].setLocation_label(unit.getLocationDescription());
-            // Price
-            cards[counter].setPrice_number(unit.getPrice());
-            // Bathrooms
-            cards[counter].setBathroom_number(unit.getBathrooms());
-            // Bedrooms
-            cards[counter].setBedroom_number(unit.getBedrooms());
-        }
-        for(ApartmentCard card : cards){
-//            System.out.println(apartmentListContainer.getComponents());
+//        ApartmentCard[] cards = new ApartmentCard[apartmentList.size()];
+//        int counter = 0;
+//        for (Unit unit : apartmentList) {
+//            // Apartment ID
+//            int apartmentID = Integer.parseInt(unit.getApartmentId());
+//            int unitID = Integer.parseInt(unit.getUnitId());
+//            cards[counter] = new ApartmentCard(apartmentID,unitID);
+//            System.out.println(unitID);
+//            // Location Description
+//            cards[counter].setLocation_label(unit.getLocationDescription());
+//            // Price
+//            cards[counter].setPrice_number(unit.getPrice());
+//            // Bathrooms
+//            cards[counter].setBathroom_number(unit.getBathrooms());
+//            // Bedrooms
+//            cards[counter].setBedroom_number(unit.getBedrooms());
+//            cards[counter].getMainPanel().setVisible(true);
+//            System.out.println(cards[counter]);
+//        }
+//        for(ApartmentCard card : cards){
+//            apartmentCards.add(new JPanel().add(card.getMainPanel()));
+//            apartmentCards.setVisible(false);
+//            apartmentCards.revalidate();
+//            apartmentCards.repaint();
+//        }
+        // Display apartmentListContainer cards ( Java version )
+//        ApartmentCardJava[] cards = new ApartmentCardJava[apartmentList.size()];
+//        int counter = 0;
+//        for (Unit unit : apartmentList) {
+//            cards[counter] = new ApartmentCardJava();
+//            // Location Description
+////            cards[counter].setLocation_label(unit.getLocationDescription());
+////            // Price
+////            cards[counter].setPrice_number(unit.getPrice());
+////            // Bathrooms
+////            cards[counter].setBathroom_number(unit.getBathrooms());
+////            // Bedrooms
+////            cards[counter].setBedroom_number(unit.getBedrooms());
+//            cards[counter].getMainPanel().setVisible(true);
+//            apartmentCards.add(cards[counter].getMainPanel());
+//            apartmentCards.setVisible(false);
+//            apartmentCards.revalidate();
+//            apartmentCards.repaint();
+//        }
 
-        }
+
+
         cardLayout.show(container,"apartmentListPage");
     }
 
     private void fetchAndFilterFromTheDB(String location, String min_price, String max_price){}
     public JPanel createMainPanel(){
         return mainPanel;
+    }
+
+
+    private void refreshApartmentCards() {
+        // Clear existing cards
+        apartmentCards.removeAll();
+
+        if (apartmentList != null) {
+            ApartmentCardJava[] cards = new ApartmentCardJava[apartmentList.size()];
+            int counter = 0;
+            for (Unit unit : apartmentList) {
+                cards[counter] = new ApartmentCardJava();
+                // Location Description
+                cards[counter].setLocation_label(unit.getLocationDescription());
+                // Price
+                cards[counter].setPrice_number(unit.getPrice());
+                // Bathrooms
+                cards[counter].setBathroom_number(unit.getBathrooms());
+                // Bedrooms
+                cards[counter].setBedroom_number(unit.getBedrooms());
+                cards[counter].getMainPanel().setVisible(true);
+                apartmentCards.add(cards[counter].getMainPanel());
+                counter++;
+            }
+        }
+
+        // Revalidate and repaint the panel to reflect the changes
+        apartmentCards.revalidate();
+        apartmentCards.repaint();
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        apartmentListContainer = new JPanel();
+        apartmentListContainer.setLayout(new BorderLayout());
+        apartmentCards = new JPanel();
+        apartmentCards.setLayout(new GridLayout(0, 4, 10, 10));
+        apartmentListContainer.add(apartmentCards, BorderLayout.CENTER);
+        for(int i = 0; i < 10; i++){
+            apartmentCards.add(new ApartmentCardJava().getMainPanel());
+        }
     }
 }
